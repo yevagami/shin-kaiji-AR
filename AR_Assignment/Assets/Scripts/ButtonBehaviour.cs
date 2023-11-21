bdcd204c8b01afee6fb9ad7d432cc23a05e94935
yevagami <antonius.adriel@gmail.com>
2023-11-21 13:29:16 -0500

    I ALWAYS COME BACK
    -Menu now works with the main Game loop
    -Changed some minor things in the ButtonBehaviour script
    -Created a resume button callback
    -Created a returnToMenu button callback 
    -Fixed an error where the score wouldn’t render when the participants had a certain amount of points. It was due to the fact that the font wasn’t able to render certain numbers (get a better font)

--- AR_Assignment/Assets/Scripts/ButtonBehaviour.cs
+++ AR_Assignment/Assets/Scripts/ButtonBehaviour.cs
@@ -7,21 +7,27 @@ public class ButtonBehaviour : MonoBehaviour {
     [Header("References")]
     [Tooltip("a reference to the selected enemy TEXT")]
     public TextMeshProUGUI selectedEnemyRef;
+
     [Tooltip("a reference to the main menu CANVAS")]
     public GameObject mainMenuCanvasRef;
+
     [Tooltip("a reference to the pause menu CANVAS")]
     public GameObject pauseMenuCanvasRef;
+
     [Tooltip("a reference to the object containing GAME")]
-    public GameObject gameRef;
+    public GameManager gameRef;
+
     [Header("Debug Reference")]
     [Tooltip("a reference to the debug strip object")]
     public DebugStripBehaviour debugStrip; // debug.log("") but on the top of the screen
-    
+
+    //I'm making these public so that I can set their values on a different scripts
     [Header("Variables")]
     [Tooltip("is the main menu shown? is it active?")]
-    [SerializeField] bool isMainMenuOpen;
+    public bool isMainMenuOpen;
+
     [Tooltip("is the pause menu shown? is it active?")]
-    [SerializeField] bool isPauseMenuOpen;
+    public bool isPauseMenuOpen;
 
     #region abstract THIS
     //  temp 'hard code' (its not a real reference)
@@ -35,14 +41,15 @@ public class ButtonBehaviour : MonoBehaviour {
     public void PlayButtonCallback() {
         if (gameRef) {
             debugStrip.log("starting game");
+            gameRef.gameObject.SetActive(true);
+            isMainMenuOpen = false;
+
             //  run the game loop
             
         } else {
             debugStrip.log("no game reference");
             isMainMenuOpen = !isMainMenuOpen;
         }
-     
-        
         
     }
 
@@ -60,6 +67,8 @@ public class ButtonBehaviour : MonoBehaviour {
 
     public void LeaveButtonCallback()
     {
+        //This caused an error when building, sorry
+        //-Adriel
         /*
         if (Application.isEditor) {
             debugStrip.log("quitting the editor");
@@ -76,6 +85,17 @@ public class ButtonBehaviour : MonoBehaviour {
 
     public void ResumeGameButtonCallback() {
         Time.timeScale = 1;
+        isPauseMenuOpen = false;
+        gameRef.GAME = true;
+    }
+
+    public void ReturnToMainButtonCallback(){
+        isPauseMenuOpen = false;
+        if (gameRef.GAME){
+            gameRef.GAME = false;
+            gameRef.gameObject.SetActive(false);
+        }
+        isMainMenuOpen = true;
     }
 
     void Start() {
@@ -83,6 +103,7 @@ public class ButtonBehaviour : MonoBehaviour {
         isPauseMenuOpen = false;
         isEnemyScanned = false;
         selectedEnemyRef.text = defaultEnemy;
+        gameRef.gameObject.SetActive(false);
         
     }
 
--- AR_Assignment/Assets/Scripts/GameManager.cs
+++ AR_Assignment/Assets/Scripts/GameManager.cs
@@ -45,11 +45,12 @@ public class GameManager : MonoBehaviour {
     [SerializeField] Transform enemyPlayPosition; //Where the enemy's selected cards are gonna be 
     [SerializeField] GameObject[] enemyCardsInScene;
 
-    [Header("Canvas stuff")]
+    [Header("UI Stuff")]
     [SerializeField] GameObject canvasUIParent; //Reference to the UI canvas 
     [SerializeReference] TextMeshProUGUI leftText;
     [SerializeReference] TextMeshProUGUI rightText;
     [SerializeReference] TextMeshProUGUI middleText;
+    [SerializeReference] ButtonBehaviour ButtonBehaviourRef;
 
     //Array containing the cards of the player and enemy
     card[] playerCards = new card[5];
@@ -73,38 +74,57 @@ public class GameManager : MonoBehaviour {
     //Debuging stuff
     int count = 0;
 
-    //For the sake of rendering the cards irl
+    //For the sake of rendering the cards in the world
     int enemyCardCount = 0;
+
+    //Bool to determine if the game has started or not
+    public bool GAME = false;
+
+    private void Awake(){
+        if(!GAME) { GAME = true; }
+    }
 
     // Update is called once per frame
     void Update() {
+        //If the game is false, exit early
+        if (!GAME) { return; }
+
+        //State machine
         switch(currentState) { 
+            //Start state
             case states.start:
-                ShuffleCards();
-                RenderEnemyCards();
-                currentState = states.idle;
+                ShuffleCards(); //Deal 5 cards to the player and the opponent. Also assign new cards if there are already cards present
+                RenderEnemyCards(); //Render the amount of cards in the enemy's hand
+                currentState = states.idle; 
                 break;
 
+            //Idle state
+            //You are only allowed to play a card in this state
             case states.idle:
-                if (CheckEndGame()){
-                    currentState = states.end;
+                if (CheckEndGame()){ //Checks if any of the winning or losing conditions have been met
+                    currentState = states.end; //If so, end the game
                 }
                 break;
 
+            //Play state
             case states.play:
-                if(playerSelected.Object != null){
-                    if (enemyPlayCard()) {
-                        renderPlayCards();
-                        RenderEnemyCards();
+                if(playerSelected.Object != null){ //Check if the player has selected a card
+                    if (enemyPlayCard()) { //Check if the enemy has played a valid card
+                        RenderPlayCards(); //Place the cards that have been selected to play in their designated position
+                        RenderEnemyCards(); //Renders the enemy's cards again to coinside with 1 less card in its hand
                         currentState = states.compare;
                     }
                 }
                 break;
 
+            //Compare state
+            //Compare the cards that have been selected and assign points based on the outcome
             case states.compare:
-                CompareCardsState();
+                CompareCardsState(); 
                 break;
-
+            
+            //Destroy state
+            //After 1 second, destroy the cards that have been played
             case states.destroy:
                 if(timer < timerDuration){
                     timer += Time.deltaTime;
@@ -115,7 +135,8 @@ public class GameManager : MonoBehaviour {
                 }
                 
                 break;
-                
+            
+            //End state
             case states.end:
                 EndGame();
                 break;
@@ -125,17 +146,12 @@ public class GameManager : MonoBehaviour {
     }
 
 
-
     //Start state
-    public void ShuffleCards()
-    {
-        //Shuffle player cards
-        for (int i = 0; i < playerCardPositions.Length; i++)
-        {
-            if (playerCards[i] != null)
-            {
-                Destroy(playerCards[i].Object);
-            }
+    public void ShuffleCards(){
+        //Assign the player cards
+        for (int i = 0; i < playerCardPositions.Length; i++){
+            //If there are already cards present, destroy them
+            if (playerCards[i] != null){ Destroy(playerCards[i].Object); }
 
             //Creating the card
             int cardIndex = Random.Range(0, 3);
@@ -146,13 +162,10 @@ public class GameManager : MonoBehaviour {
 
 
         //Shuffle the enemy's cards
+        //Basically the same principal as the player's cards
         enemyCardCount = 0;
-        for (int i = 0; i < enemyCardPositions.Length; i++)
-        {
-            if (enemyCards[i] != null)
-            {
-                Destroy(enemyCards[i].Object);
-            }
+        for (int i = 0; i < enemyCardPositions.Length; i++){
+            if (enemyCards[i] != null){ Destroy(enemyCards[i].Object); }
 
             int cardIndex = Random.Range(0, 3);
             enemyCards[i] = new card(Instantiate(cards[cardIndex], enemyCardPositions[i].position, Quaternion.identity), cardIndex, i);
@@ -170,48 +183,41 @@ public class GameManager : MonoBehaviour {
 
     //Play state
     public void PlayCard(int cardPos){
-            //Selecting cards are only allowed on the play state
-            if (currentState != states.idle){
-                return;
-            }
-       
-            playerSelected = playerCards[cardPos];
-            currentState = states.play;
+        //Selecting cards are only allowed on the play state
+        if (currentState != states.idle){
+            return;
+        }
+        
+        playerSelected = playerCards[cardPos];
+        currentState = states.play;
     }
-    bool enemyPlayCard()
-    {
+    bool enemyPlayCard(){
         int selectedCard = Random.Range(0, enemyCards.Length); //Randomly pick which card for the enemy to play
 
         //Picks another random card if that specific card has been played
-        if (enemyCards[selectedCard] == null)
-        {
-            return false;
-        }
+        if (enemyCards[selectedCard] == null) { return false; }
 
         enemyCardCount -= 1;
         enemySelected = enemyCards[selectedCard];
         return true;
-    }
-    void renderPlayCards()
-    {
+    }  
+    void RenderPlayCards(){
         playerSelected.Object.transform.position = playerPlayPosition.transform.position;
         playerSelected.Object.transform.localScale = playerPlayPosition.transform.localScale;
 
         enemySelected.Object.transform.position = enemyPlayPosition.transform.position;
         enemySelected.Object.transform.localScale = enemyPlayPosition.transform.localScale;
         enemySelected.Object.transform.SetParent(canvasUIParent.transform);
-    }
+    } 
 
     //Compare state
-    void CompareCardsState()
-    {
+    void CompareCardsState(){
         condition result = CompareCards(playerSelected, enemySelected);
         if(result == condition.lose){
             enemyScore += 1;
         }
 
-        if(result == condition.win)
-        {
+        if(result == condition.win){
             playerScore += 1;
         }
         
@@ -281,6 +287,7 @@ public class GameManager : MonoBehaviour {
     //End state
     void EndGame(){
         middleText.text = CheckEndGameCondition().ToString();
+        GAME = false;
     }
 
     //Reset the game back to normal
@@ -292,6 +299,7 @@ public class GameManager : MonoBehaviour {
         leftText.text = "Player " + playerScore.ToString();
         rightText.text = enemyScore.ToString() + " Enemy";
 
+        GAME = true;
         currentState = states.start;
     }
 
@@ -304,5 +312,19 @@ public class GameManager : MonoBehaviour {
         for(int i = 0; i < enemyCardCount; i++){
             enemyCardsInScene[i].SetActive(true);
         }
+    }
+
+    //Exit game
+    public void ExitGame(){
+        Reset();
+        GAME = false;
+        gameObject.SetActive(false);
+    }
+
+    //Pauses the game
+    public void PauseGame(){
+        Time.timeScale = 0;
+        GAME = false;
+        ButtonBehaviourRef.isPauseMenuOpen = true;
     }
 }
